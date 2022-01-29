47b6c903d168098b66e1bff416c3dfa29150cf7e
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-29 12:30:05 -0300

    Criada estrutura de repetição

--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -4,30 +4,44 @@ public class Order {
 
     private String code;
     private final int totalValue;
+    private String[] itens;
 
     public Order(String code, int totalValue) {
         this.code = code;
         this.totalValue = totalValue;
     }
 
-//    public double calculateFee() {
-//        if(this.totalValue > 100) {
-//            return this.totalValue * 0.99;
-//        } else {
-//            return this.totalValue;
+    // While
+//    public void printItens() {
+//        int i = 0;
+//        while(i < itens.length) {
+//            System.out.println(itens[i]);
+//            i++;
 //        }
 //    }
 
-        public double calculateFee() {
-        switch (totalValue) {
-            case 100:
-                    return totalValue * 0.99;
-            case 200:
-                    return totalValue * 1.99;
-            default:
-                return totalValue;
+    // Do While
+//    public void printItens() {
+//        int i = 0;
+//
+//        do {
+//            System.out.println(itens[i]);
+//            i++;
+//        }while(i < itens.length);
+//    }
+
+    // For
+//    public void printItens() {
+//        for(int i = 0; i < itens.length; i ++) {
+//            System.out.println(itens[i]);
+//        }
+//    }
+
+    // Enhanced for
+    public void printItens() {
+        for(String i : itens) {
+            System.out.println(i);
         }
     }
-
 
 }
