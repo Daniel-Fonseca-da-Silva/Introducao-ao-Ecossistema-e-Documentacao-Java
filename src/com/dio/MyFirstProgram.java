a5dc55622aa6a6d368b5ead87cc458ece99b048a
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-29 12:57:20 -0300

    Criada documentação(Tags javadoc)

--- src/com/dio/MyFirstProgram.java
+++ src/com/dio/MyFirstProgram.java
@@ -2,9 +2,11 @@ package com.dio;
 
 import com.dio.base.Order;
 
+import java.math.BigDecimal;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
-        Order order = new Order("Code1234", 199);
+        Order order = new Order("Code1234", new BigDecimal("100.00"));
 
         System.out.println(order.calculateFee());
 
--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -1,52 +1,42 @@
 package com.dio.base;
 
-// Classe order
+import java.math.BigDecimal;
+
+/**
+ *  @author Daniel Fonseca
+ *  @version 1.0.0
+ *  @see BigDecimal
+ *  @since Release 1.0.0
+ */
 public class Order {
 
+    // Representa o código do pedido
     private String code;
-    private final int totalValue;
-    private String[] itens;
+    private final BigDecimal totalValue;
 
-    public Order(String code, int totalValue) {
+    /**
+     *  Construtor da classe
+     *  @param  code        Código do pedido
+     *  @param  totalValue  valor total do pedido
+     */
+    public Order(String code, BigDecimal totalValue) {
         this.code = code;
         this.totalValue = totalValue;
     }
 
     /**
-     * Comentário sobre while, do while, for e enhanced for
-     */
-
-    // While
-//    public void printItens() {
-//        int i = 0;
-//        while(i < itens.length) {
-//            System.out.println(itens[i]);
-//            i++;
-//        }
-//    }
-
-    // Do While
-//    public void printItens() {
-//        int i = 0;
-//
-//        do {
-//            System.out.println(itens[i]);
-//            i++;
-//        }while(i < itens.length);
-//    }
-
-    // For
-//    public void printItens() {
-//        for(int i = 0; i < itens.length; i ++) {
-//            System.out.println(itens[i]);
-//        }
-//    }
-
-    // Enhanced for
-    public void printItens() {
-        for(String i : itens) {
-            System.out.println(i);
+     *  Calcula o total de acordo com o valor do pedido. Se o valor for maior que R$100,00 uma taxa será cobrada
+     * @return  valor total do pedido com as taxas
+     * @throws  RuntimeException    Se o valor do pedido for negativo
+     * */
+    public BigDecimal calculateFee() throws RuntimeException {
+        if(this.totalValue.signum() < 0) {
+            throw new RuntimeException("O pedido não pode ter valor negativo");
         }
+        if(this.totalValue.compareTo(new BigDecimal("100.00")) > 100) {
+            return this.totalValue.multiply(new BigDecimal("0.99"));
+        }
+        return this.totalValue;
     }
 
 }
